d2e82e4ac77edf937180874bfd46ea15aed92bd2
Mateusz Stodółkiewicz <mateusz.stodolkiewicz@monterail.com>
2022-09-13 11:35:46 +0200

    Update validation for Seance

--- app/models/seance.rb
+++ app/models/seance.rb
@@ -8,6 +8,18 @@ class Seance < ApplicationRecord
     self.end_time = self.start_time + self.movie.duration.minutes + 30.minutes
   end
 
+  validate :is_used
+
+  def is_used
+    Seance.where(hall_id: self.hall_id).each do |item|
+      if self.start_time.between?(item.start_time, item.end_time) ||
+          self.end_time.between?(item.start_time, item.end_time)
+        errors.add(:start_time, "Hall is used for another seance")
+        break
+      end
+    end
+  end
+
   validates_associated :hall, :movie
 
   validates :start_time, :price, :end_time, presence: true
