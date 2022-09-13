8408f07b5ff6fc9217f4076ce8957ddb86ac3079
Mateusz Stodółkiewicz <mateusz.stodolkiewicz@monterail.com>
2022-09-13 18:16:22 +0200

    fix methods

--- app/models/seance.rb
+++ app/models/seance.rb
@@ -2,27 +2,30 @@ class Seance < ApplicationRecord
   belongs_to :hall
   belongs_to :movie
 
-  before_validation :end_time
+  before_validation :set_end_time
 
-  def end_time
-    self.end_time = self.start_time + self.movie.duration.minutes + 30.minutes
+  def set_end_time
+    self.end_time = start_time + movie.duration.minutes + 30.minutes
   end
 
-  validate :is_used
-
-  def is_used
-    Seance.where(hall_id: self.hall_id).each do |item|
-      if self.start_time.between?(item.start_time, item.end_time) ||
-          self.end_time.between?(item.start_time, item.end_time)
-        errors.add(:start_time, "Hall is used for another seance")
-        break
-      end
-    end
-  end
+  validate :used?
 
   validates_associated :hall, :movie
 
   validates :start_time, :price, :end_time, presence: true
   validates :end_time, comparison: { greater_than: :start_time }
   validates :price, numericality: { greater_than: 0 }
+  
+  private
+
+  def used?
+    unless Seance.where(hall_id: hall_id)
+      .where(end_time: start_time..end_time)
+      .or(Seance.where(hall_id: hall_id)
+      .where(start_time: start_time..end_time))
+      .empty?
+      errors.add(:start_time, "Hall is used for another seance")
+    end
+  end
+  
 end
