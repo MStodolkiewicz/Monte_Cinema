8d52183a885d30fa9113a83a40ed162fe3a03f28
Mateusz Stodółkiewicz <mateusz.stodolkiewicz@monterail.com>
2022-09-19 12:46:11 +0200

    update validation

--- app/models/seance.rb
+++ app/models/seance.rb
@@ -2,21 +2,23 @@ class Seance < ApplicationRecord
   belongs_to :hall
   belongs_to :movie
 
-  before_validation :set_end_time
-
-  def set_end_time
-    self.end_time = start_time + movie.duration.minutes + 30.minutes
-  end
+  validates_associated :hall, :movie
+  before_validation :set_end_time, unless: :start_nil?
 
   validate :used?
-
-  validates_associated :hall, :movie
-
   validates :start_time, :price, :end_time, presence: true
   validates :end_time, comparison: { greater_than: :start_time }
   validates :price, numericality: { greater_than: 0 }
 
   private
+
+  def start_nil?
+    start_time.nil?
+  end
+
+  def set_end_time
+    self.end_time = start_time + movie.duration.minutes + TIME_OF_ADS
+  end
 
   def used?
     return if scence_within_given_time_and_hall_exist?
