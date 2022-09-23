cabf9eb3bbe4e763f3a65f932685b52356e772c8
Mateusz Stodółkiewicz <mateusz.stodolkiewicz@monterail.com>
2022-09-23 16:01:51 +0200

    add password validation

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,4 +1,4 @@
 class Discount < ApplicationRecord
-  validates :tickets_needed, :value, presence: true
-  validates :tickets_needed, :value, numericality: { only_integer: true, greater_than: 0 }
+  validates :tickets_needed, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :value, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
   has_many :seances, dependent: :destroy
 
-  validates :name, :duration, presence: true
-  validates :duration, numericality: { only_integer: true, greater_than: 0 }
+  validates :name, presence: true
+  validates :duration, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
