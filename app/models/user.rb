1578e5d86fd2f06ec88c687e46e62eaa712689ad
Mateusz Stodółkiewicz <mateusz.stodolkiewicz@monterail.com>
2022-09-26 13:15:34 +0200

    rubocop fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,10 @@ class User < ApplicationRecord
   validate :validate_password_length
 
   private
+
   def validate_password_length
-    return unless  password.nil? || password.bytesize > 72
-      errors.add(:password, 'Password is too long') 
+    return unless password.nil? || password.bytesize > 72
+
+    errors.add(:password, 'Password is too long')
   end
 end
