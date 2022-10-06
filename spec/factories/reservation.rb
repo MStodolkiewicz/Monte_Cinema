4b6b060dd9037e9dab0394e4d16f4b722db073e2
Mateusz Stodółkiewicz <mateusz.stodolkiewicz@monterail.com>
2022-10-06 14:59:07 +0200

    add spec

---
+++ spec/factories/reservation.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+    factory :reservation do
+      email { "test@test.com" }
+      status { :reserved }
+      association :seance, strategy: :build
+      association :user, strategy: :build
+    end
+  end
+  
