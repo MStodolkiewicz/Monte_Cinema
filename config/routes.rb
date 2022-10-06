6faf104812b91b772c47ad2e3fe94527c98e5fd3
Mateusz Stodółkiewicz <mateusz.stodolkiewicz@monterail.com>
2022-10-06 13:16:01 +0200

    config sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+require 'sidekiq/web'
+require 'sidekiq/cron/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
   devise_for :users
   root 'movies#index'
   resources :movies do
