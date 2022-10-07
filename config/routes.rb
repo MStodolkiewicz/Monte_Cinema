b76f23edb15a95f0a86b54c2b910b0c474231c66
Mateusz Stodółkiewicz <mateusz.stodolkiewicz@monterail.com>
2022-10-07 18:47:38 +0200

    secure sidekiq endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ require 'sidekiq/web'
 require 'sidekiq/cron/web'
 
 Rails.application.routes.draw do
-  mount Sidekiq::Web => '/sidekiq'
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
   devise_for :users
   root 'movies#index'
   resources :movies do
