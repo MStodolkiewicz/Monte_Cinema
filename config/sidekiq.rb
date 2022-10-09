787850be1b01bd5181e36986b7ec73773d226ae0
Mateusz Stodółkiewicz <mateusz.stodolkiewicz@monterail.com>
2022-10-09 15:49:58 +0200

    change sidekiq config

--- config/sidekiq.rb
+++ config/sidekiq.rb
@@ -1,12 +1,14 @@
 Sidekiq.configure_server do |config|   
-    config.redis = { url: 'redis://127.0.0.1:6379' } 
+  config.redis = { url: 'redis://127.0.0.1:6379' } 
+  config.on(:startup) do
+    schedule_file = 'config/schedule.yml'
+
+    if File.exist?(schedule_file) && Sidekiq.server?
+      Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+    end
   end
-  Sidekiq.configure_client do |config|   
-    config.redis = { url: 'redis://127.0.0.1:6379' } 
-  end
-  
-  schedule_file = 'config/schedule.yml'
-  if File.exist?(schedule_file) && Sidekiq.server?
-    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
-  end
-  
+end
+
+Sidekiq.configure_client do |config|   
+  config.redis = { url: 'redis://127.0.0.1:6379' } 
+end
