4e7808e97681e97c3136ce65f549e7476a1b79ca
Mateusz Stodółkiewicz <mateusz.stodolkiewicz@monterail.com>
2022-10-12 01:18:58 +0200

    test fixes

--- spec/factories/discount.rb
+++ spec/factories/discount.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
-    factory :discount do
-      tickets_needed { Faker::Number.number(digits: 1) }
-      value { Faker::Number.number(digits: 2) }
-    end
+  factory :discount do
+    tickets_needed { Faker::Number.number(digits: 1) }
+    value { Faker::Number.number(digits: 2) }
   end
-  
+end
